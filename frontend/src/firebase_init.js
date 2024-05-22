65be36cddff4ef66ee0a4ce0825da433ef82a4d4
ShamsQamar <89519299+ShamsQamar@users.noreply.github.com>
2024-05-23 02:46:18 +0530

    Update firebase_init.js
    Removing sensitive information

--- frontend/src/firebase_init.js
+++ frontend/src/firebase_init.js
@@ -4,12 +4,7 @@ import { getMessaging } from "firebase/messaging";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyATWk7AbDXVreGLZ958UjyWxZ5jrVx1UKU",
-    authDomain: "twitter-clone-b58e9.firebaseapp.com",
-    projectId: "twitter-clone-b58e9",
-    storageBucket: "twitter-clone-b58e9.appspot.com",
-    messagingSenderId: "253472742662",
-    appId: "1:253472742662:web:8473928e3e39f266da5287"
+    // put your auth details for configuration 
 };
 
 // Initialize Firebase
